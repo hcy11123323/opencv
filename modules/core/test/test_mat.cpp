8bda049f2c2aa04eb1edea82698796dc02fe031f
胡晨宇 <12410918@mail.sustech.edu.cn>
2026-05-25 22:51:10 +0800

    add a simple test case

--- modules/core/test/test_mat.cpp
+++ modules/core/test/test_mat.cpp
@@ -2625,6 +2625,23 @@ TEST(Mat1D, DISABLED_basic)
         EXPECT_EQ(0, pt.y);
     }
 }
+TEST(Mat, regression_cvReshapeMatND_continuous)
+{
+    int sizes[] = {2, 3, 4};
+    Mat mat(3, sizes, CV_32SC1);
+    CvMatND src = cvMatND(mat);
+    CvMatND reshaped;
+    int new_sizes[] = {4, 3, 2};
+    CvArr* result = 0;
+
+    ASSERT_NO_THROW(result = cvReshapeMatND(&src, sizeof(reshaped), &reshaped, 0, 3, new_sizes));
+    ASSERT_NE((CvArr*)0, result);
+    EXPECT_EQ(3, reshaped.dims);
+    EXPECT_EQ(new_sizes[0], reshaped.dim[0].size);
+    EXPECT_EQ(new_sizes[1], reshaped.dim[1].size);
+    EXPECT_EQ(new_sizes[2], reshaped.dim[2].size);
+    EXPECT_EQ(src.data.ptr, reshaped.data.ptr);
+}
 
 TEST(Mat, ptrVecni_20044)
 {
